884b55a764dfdf85cd34a0f75815e39710c4ec10
ste26054 <ste26054@gmail.com>
2015-10-01 15:12:21 +0100

    add several graphs in view for test queries

--- app/controllers/re_graphs_controller.rb
+++ app/controllers/re_graphs_controller.rb
@@ -1,8 +1,10 @@
 class ReGraphsController < ApplicationController
   unloadable
+  menu_item :redmine_graphs
 
   def index
-
+    @project = Project.find(params[:project_id])
+    @data = {"Monday"=>37, "Tuesday"=>23, "Wednesday"=>7, "Thursday"=>11, "Friday"=>9, "Saturday"=>2}
   end
 
 end
